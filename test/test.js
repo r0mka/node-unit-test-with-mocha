10d20dd162ebfba620b2c939cc0fd35c17946a99
Roman <romanliveshere@gmail.com>
2021-06-26 23:18:51 -0700

    initial commit

---
+++ test/test.js
@@ -0,0 +1,9 @@
+const assert = require('assert');
+
+describe('file to be tested', () => {
+  context('function to be tested', () => {
+    it('should do something', () => {
+      assert.strictEqual(1, 2);
+    });
+  });
+});
