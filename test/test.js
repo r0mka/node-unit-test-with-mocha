741899a38f4bfc9123cc14c4f7e31b67eaf82ad8
Roman <romanliveshere@gmail.com>
2021-06-26 23:37:03 -0700

    add some demo tests

--- test/test.js
+++
@@ -1,9 +0,0 @@
-const assert = require('assert');
-
-describe('file to be tested', () => {
-  context('function to be tested', () => {
-    it('should do something', () => {
-      assert.strictEqual(1, 2);
-    });
-  });
-});
---
+++ tests/test.js
@@ -0,0 +1,25 @@
+const assert = require('assert');
+
+describe('file to be tested', () => {
+  context('function to be tested', () => {
+    beforeEach(() => {
+      console.log('======before');
+    });
+    afterEach(() => {
+      console.log('======after');
+    });
+    it('should do something', () => {
+      assert.strictEqual(1, 1);
+    });
+
+    it('should do something else', () => {
+      assert.deepStrictEqual({ name: 'Roman' }, { name: 'Roman' });
+    });
+  });
+
+  context('anothere function to be tested', () => {
+    it('should do something else', () => {
+      assert.deepStrictEqual({ name: 'Roman' }, { name: 'Roman' });
+    });
+  });
+});
